a3a658d5b572d338c084f590aa0364a85b9aea94
Greta Pfundt <g.pfundt325@gmail.com>
2019-09-19 18:11:35 -0400

    final with comments

--- pyBank/main.py
+++ pyBank/main.py
@@ -1,13 +1,16 @@
 import os
 import csv
 
-csvpath = os.path.join("./Resources/budget_data.csv")
-print(csvpath)
+#define file path
+csvpath = os.path.join("Resources/budget_data.csv")
 
+#open and read csv file 
 with open(csvpath, newline = "") as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
     csvheader = next(csvreader)
     
+    #make a list of money and months
+    #find total money by adding each component of the money list
     totalMoney = 0
     money_l = []
     months_l = []
@@ -15,33 +18,41 @@ with open(csvpath, newline = "") as csvfile:
         totalMoney = totalMoney + int(row[1])
         money_l.append(row[1])
         months_l.append(row[0])
+    #find the total months
     totalMonths = len(months_l)
     
+    #change money list from a list of strings to a list of integers
     for i in range(0, len(money_l)): 
         money_l[i] = int(money_l[i])
     
+    #find the list of changes by subtracting the next value from the first value 
     change_l = []
     change = 0
     for num in range(0,(len(money_l)-1)):
         change = money_l[num + 1] - money_l[num]
         change_l.append(change)
 
+    #find the min, max, and average of the changes
     gIncrease = max(change_l)
     gDecrease = min(change_l)
     avgChange = sum(change_l)/len(change_l)
     
+    #find the dates that correspond to the max and min changes
     date1 = (change_l.index(gIncrease))+1
     date2 = (change_l.index(gDecrease))+1
     
+    #find the dates that correspond to the max and min changes
     incDate = months_l[int(date1)]
     decDate = months_l[int(date2)]
     
+    #print the results
     print(totalMonths)
     print(totalMoney)
     print(avgChange)
     print("Greatest increase in Profits: {} (${})".format(incDate,gIncrease))
     print("Greatest decrease in Profits: {} (${})".format(decDate,gDecrease))
-    
+
+ #create a text file and print the results   
 newfilepath = "hwOutput.txt"
 with open(newfilepath,"w") as txtfile:   
     txtfile.write("Financial Analysis")
